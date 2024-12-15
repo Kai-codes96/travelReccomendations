7f87d311c09d11ea544ce714d4f91e0343796671
kai-codes96 <Kayinwalker16@Gmail.com>
2024-12-15 17:53:27 -0500

    travelRecommendation

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -10,4 +10,7 @@ function clear() {
     document.getElementById("search").value= "";
 }
 
-fetch ('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json');.then(response => response.json());
+fetch ('travel_recommendation_api.json').then(response => response.json()).then(data => {
+    const showPlaces= document.getElementById("showPlaces");
+    const searchField= document.getElementById("search").value.toLowerCase();
+})
